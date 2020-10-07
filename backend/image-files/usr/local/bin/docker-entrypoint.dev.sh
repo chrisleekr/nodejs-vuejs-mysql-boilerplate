4f00091cf4f94638c07ef8b03b8a8cd7c74744c3
Chris Lee <git@chrislee.kr>
2020-10-07 21:37:10 +1100

    Revert some changes

---
+++ backend/image-files/usr/local/bin/docker-entrypoint.dev.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+mkdir -p /srv/static
+
+cat <<EOT > /srv/public/static/config.json
+{
+  "apiUrl": "$API_URL",
+  "format": {
+    "timeZone": "Australia/Melbourne",
+    "dateTime": "YYYY-MM-DD HH:mm:ss",
+    "pickerDateTime": "yyyy-MM-dd HH:mm"
+  }
+}
+EOT
+
+exec "$@"
