4a5cf556840766b5ba2053fd7de43a7f87c1f5b7
chrisleekr <chrisleekr@users.noreply.github.com>
2020-11-28 18:33:14 +1100

    Separated migration (#21)

--- api/image-files/usr/local/bin/docker-entrypoint.sh
+++ api/image-files/usr/local/bin/docker-entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 echo "   _____ __________.___"
 echo "  /  _  \\______   \   |"
 echo " /  /_\  \|     ___/   |"
@@ -8,24 +7,6 @@ echo "/    |    \    |   |   |"
 echo "\____|__  /____|   |___|"
 echo "        \/"
 
-
 set -e
-
-printf "Checking database connection...\n\n"
-mysql_ready() {
-    mysqladmin ping --host="$DB_HOST" --user="$DB_USER" --password="$DB_PASSWORD" > /dev/null 2>&1
-}
-
-while ! (mysql_ready)
-do
-    sleep 3
-    echo "Waiting for database connection ..."
-done
-
-echo "Database is ready..."
-
-echo "Starting migration..."
-npm run migrate:up
-echo "Finished migration..."
 
 exec "$@"
